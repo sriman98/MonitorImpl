96880345e74843a35c47f1ed8b6fd2bb68baf726
sriman ch <srimans65@gmail.com>
2021-11-13 14:31:26 -0600

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,100 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class Account
+{
+    Lock lock= new ReentrantLock();
+
+    //two conditions, 1st one for when deposit occurs and second one for giving preference to preferred withdrawals
+    Condition condition=lock.newCondition();
+    Condition condition2=lock.newCondition();
+
+    int balance;
+
+    //Getting a count of preferred withdrawals in the queue
+    int preferredWithdraws=0;
+
+    public Account()
+    {balance=0;}
+
+    public Account(int balance)
+    {this.balance=balance;}
+
+
+    //Deposit and signal when deposit is done
+    public void deposit(int k)
+    {
+        lock.lock();
+
+        balance+=k;
+        System.out.println("Deposit Successfull, New Balance ="+balance);
+        condition.signal();
+
+        lock.unlock();
+    }
+
+    //withdrawal method to withdraw based on preferred or ordinary transaction
+    public void withdraw(int k,boolean isPreferred)
+    {
+        if(isPreferred)
+        {preferredWithdraws++;}
+
+        lock.lock();
+
+
+        while (balance < k)
+        {
+            try
+            {
+                condition.await();
+            }
+            catch (InterruptedException e)
+            {System.out.println("Interrupted");}
+
+            if(!isPreferred)
+            {
+                while(preferredWithdraws>0)
+                {
+                    try
+                    {condition2.await();}
+                    catch (InterruptedException e)
+                    {System.out.println("Interrupted");}
+                }
+            }
+        }
+
+        balance-=k;
+
+
+
+        System.out.println("Withdraw Successfull, New Balance ="+balance);
+
+        if(isPreferred)
+        {
+            preferredWithdraws--;
+            condition2.signalAll();
+         }
+
+
+        lock.unlock();
+    }
+
+
+    //transfer function as defined in 3rd part
+    void transfer(int k, Account reserve) {
+        lock.lock();
+        try {
+            reserve.withdraw(k,false);
+            deposit(k);
+        } finally {
+            lock.unlock();
+        }
+
+        System.out.println("Transaction Successfull");
+    }
+
+
+
+}
---
+++ src/TransactMain.java
@@ -0,0 +1,42 @@
+public class TransactMain {
+
+
+    public static void main(String[] args)
+    {
+        Account account= new Account(0);
+       Runnable r1=  new Runnable() {
+            @Override
+            public void run() {
+                account.withdraw(100,true);
+                account.deposit(100);
+                account.withdraw(400,true);
+            }
+        };
+        Runnable r2= new Runnable() {
+            @Override
+            public void run() {
+                account.deposit(200);
+                account.withdraw(100,true);
+                account.deposit(300);
+            }
+        };
+        Runnable r3=new Runnable() {
+            @Override
+            public void run() {
+                account.deposit(100);
+                account.withdraw(200,false);
+                account.deposit(100);
+                account.withdraw(100,false);
+            }
+        };
+
+        Thread t1=new Thread(r1);
+        Thread t2= new Thread(r2);
+        Thread t3= new Thread(r3);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
